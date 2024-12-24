c4501efa0d641295f09c0e974e9d98036ee59ff8
DoggieLicc <doggie@doggieli.cc>
2024-12-24 15:21:15 -0500

    Fix skin loading

--- RobloxFiles/StarterPlayer/StarterPlayerScripts/SkinLoader.LocalScript.lua
+++ RobloxFiles/StarterPlayer/StarterPlayerScripts/SkinLoader.LocalScript.lua
@@ -1,24 +1,23 @@
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
 local remote_img = require(game.ReplicatedStorage.remote_img)
+local asset_service = game:GetService("AssetService");
+
 
 ReplicatedStorage:WaitForChild("loadPlayerSkin").OnClientEvent:Connect(function(uuid, player, Character)
 
 	local skin = remote_img.create_image("https://crossplayproject.xyz/api/uuid/"..uuid.."/skin")
 	task.wait()
 	for _, part in pairs(Character.SecondLayer:GetChildren()) do
-
 		if part:IsA("MeshPart") then
-
-			local imageClone = skin:Clone()
-			imageClone.Parent = part
+			part.TextureContent = Content.fromObject(skin)
 		end
 	end
+
 	for _, part in pairs(Character:GetChildren()) do
 		if part:IsA("MeshPart") then
-
-			local imageClone = skin:Clone()
-			imageClone.Parent = part
+			part.TextureContent = Content.fromObject(skin)
 		end
 	end
+
 end)
